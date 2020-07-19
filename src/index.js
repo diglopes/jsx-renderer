0a2625d78bd01f1af9f12bb70c9714120f8646ac
Diego Lopes <diego-chez@hotmail.com>
2020-07-19 16:10:50 -0300

    feat: add more jsx elements to the main vnode

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ function hyperscript(nodeName, attributes, ...args) {
 }
 
 /** @jsx hyperscript */
-let node = <div id="Diego">Front end developer</div>;
-console.log("Node", node);
+let vnode = (
+  <div id="jsx">
+    <h1>Building a JSX renderer</h1>
+    <h2>By Diego lopes - Front end developer</h2>
+
+    <p>
+      This is an HTML page with a single babel-transpiled JS file and no
+      dependencies. It is rendering DOM via JSX without any frameworks.
+    </p>
+  </div>
+);
